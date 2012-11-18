ce720ce2385a58e5f77f622339f70af4d262c37b
Jeroen Coenders <jeroencoenders@gmail.com>
2012-11-18 15:05:26 +0100

    Added geometrical tests

--- src/tests/geometry-tests.py
+++ src/tests/geometry-tests.py
@@ -18,12 +18,23 @@ class Point2dTests(unittest.TestCase):
         point=geometry.Point2d(0.0,0.0)
         self.assertEqual(str(point), 'Point2d')
 
+    def test_coordinates(self):
+        point=geometry.Point2d(1.0,2.0)
+        self.assertEqual(point.x, 1.0)
+        self.assertEqual(point.y, 2.0)
+
 class Point3dTests(unittest.TestCase):
     """Tests for Point3d"""
 
     def test_unicode(self):
         point=geometry.Point3d(0.0,0.0,0.0)
         self.assertEqual(str(point), 'Point3d')
+
+    def test_coordinates(self):
+        point=geometry.Point3d(1.0,2.0,3.0)
+        self.assertEqual(point.x, 1.0)
+        self.assertEqual(point.y, 2.0)
+        self.assertEqual(point.z, 3.0)
 
 """Set up the test suite"""
 current_module = sys.modules[__name__]

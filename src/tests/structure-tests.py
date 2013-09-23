aed62ce89accf421b4308500d62b617af2106b87
jeroencoenders <jeroencoenders@gmail.com>
2013-09-23 22:48:31 +0200

    (fail) Added tests for constraints on Node2d

--- src/tests/structure-tests.py
+++ src/tests/structure-tests.py
@@ -112,6 +112,14 @@ class Node2dTests(unittest.TestCase):
     def test_coordinates(self):
         self.assertEqual(self.node.x, 1.0)
         self.assertEqual(self.node.y, 2.0)
+    
+    def test_constraints(self):
+        self.assertEqual(self.node.cx, False)
+        self.assertEqual(self.node.cy, False)
+        
+        node1=Node2d('004',1.0,2.0,True,True)
+        self.assertEqual(node1.cx, True)
+        self.assertEqual(node1.cy, True)
 
 class Element2dTests(unittest.TestCase):
     """Tests for Element2d"""

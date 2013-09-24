8c2e33d558178c2ea263281a13c59118f5832d27
jeroencoenders <jeroencoenders@gmail.com>
2013-09-24 17:56:10 +0200

    (success) Small fix in FEM2d code

--- src/algorithms/fem2d.py
+++ src/algorithms/fem2d.py
@@ -87,7 +87,7 @@ class Fem2d(CoreClass):
             fy[nnr]+=load.Y
             ft[nnr]+=load.T
             
-        # Construct the stiffness matrix
+        # Construct the global stiffness matrix
         for i in range(nm):
             member=self.structure.e[i]
             kk=self.structure.findNode(member.endnode)
@@ -139,7 +139,7 @@ class Fem2d(CoreClass):
                     mm1 = ns[m]
                     s[kk1][mm1]= s[kk1][mm1]+sl[k][m]
             
-        #Construction of the global stiffness vector
+        #Construction of the global force vector
         for i in range(nn):
             kx = 3 * i
             ky = 3 * i + 1
